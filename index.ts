8730f1c0e73f93eb61d9481ae17e892ee09bfb71
Leszek Synowiec <leszek.synowiec@gmail.com>
2023-04-27 18:36:32 +0200

    init project

---
+++ index.ts
@@ -0,0 +1,19 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from 'dotenv';
+import path from 'path';
+
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req: Request, res: Response) => {
+    res.sendFile(`${__dirname}/public/index.html`)
+})
+
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+})
